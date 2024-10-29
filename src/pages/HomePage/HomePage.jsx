6d37ca96384636d76ad331bc24b2b94f162a0e0d
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-10-28 18:58:09 -0600

    created the HomePage and Upload pages jsx and start setting the router dom

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './HomePage.scss';
+import Hero from '../../components/Hero/Hero';
+import Description from '../../components/Description/Description';
+import VideoList from '../../components/VideoList/VideoList';
+import CommentsList from '../../components/CommentsList/CommentsList';
+import data from '../../data/video-details.json';
+
+function HomePage() {
+  const [videoData, setVideoData] = useState(data);
+  const [activeVideoId, setActiveVideoId] = useState(videoData[0].id);
+
+  const changeActiveVideo = (id) => {
+    setActiveVideoId(id);
+  };
+  const selectedVideo =
+    videoData.find((video) => video.id === activeVideoId) || videoData[0];
+
+  const useTimeStamp = (timeStamp) => {
+    const currentDate = new Date();
+
+    const sec = Math.floor((currentDate - timeStamp) / 1000);
+    const min = Math.floor(sec / 60);
+    const hours = Math.floor(min / 60);
+    const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
+    const month = Math.floor(weeks / 4);
+    const years = Math.floor(month / 12);
+
+    if (years >= 1) {
+      return `${years} years ago`;
+    } else if (month >= 1) {
+      return `${month} month ago`;
+    } else if (weeks >= 1) {
+      return `${weeks} weeks ago`;
+    } else if (days >= 1) {
+      return `${days} days ago`;
+    } else if (hours >= 1) {
+      return `${hours} hours ago`;
+    } else if (min >= 1) {
+      return `${min} minutes ago`;
+    } else {
+      return `just now`;
+    }
+  };
+  return (
+    <>
+      <main>
+        <Hero
+          videoUrl={selectedVideo.video}
+          posterImage={selectedVideo.image}
+        />
+        <div className="bottom">
+          <div className="bottom__block1">
+            <Description video={selectedVideo} timeStamp={useTimeStamp} />
+            <CommentsList
+              comments={selectedVideo.comments}
+              timeStamp={useTimeStamp}
+            />
+          </div>
+          <div className="bottom__block2">
+            <VideoList
+              data={videoData}
+              activeVideoId={activeVideoId}
+              changeActiveVideo={changeActiveVideo}
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
