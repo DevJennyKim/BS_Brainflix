ef802c3277370e1b138192d82988f3af40746da4
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-01 16:51:14 -0600

    fix the error on app.jsx as I removed the scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './App.scss';
+
 import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import Upload from './pages/Upload/Upload';
