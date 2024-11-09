966adf0e7c7913b3d9412d4d9f1944ca353970dd
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-09 00:09:12 -0700

    video player basic button setup

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,18 @@
 import './Hero.scss';
-import play from '../../assets/icons/play.svg';
-import pause from '../../assets/icons/pause.svg';
-import fullscreen from '../../assets/icons/fullscreen.svg';
-import close_fullscreen from '../../assets/icons/close_fullscreen.svg';
-import scrub from '../../assets/icons/scrub.svg';
+import playIcon from '../../assets/icons/play.svg';
+import pauseIcon from '../../assets/icons/pause.svg';
+import fullscreenIcon from '../../assets/icons/fullscreen.svg';
+import close_fullscreenIcon from '../../assets/icons/close_fullscreen.svg';
+import scrubIcon from '../../assets/icons/scrub.svg';
+import { useRef, useState } from 'react';
 
 function Hero({ videoUrl, posterImage }) {
+  const [range, setRange] = useState(0);
+  const play = useRef();
+  const pause = useRef();
+  const fullscreen = useRef();
+  const close_fullscreen = useRef();
+  const scrub = useRef();
   return (
     <section className="hero">
       <video className="hero__video" poster={posterImage}>
@@ -13,28 +20,28 @@ function Hero({ videoUrl, posterImage }) {
         Sorry, your browser doesn't support the video.
       </video>
       <div className="hero__video-controls">
-        <button className="hero__video-play">
-          <img src={play} alt="play" />
+        <button className="hero__video-play" ref={play}>
+          <img src={playIcon} alt="play" />
         </button>
-        <button className="hero__video-pause">
-          <img src={pause} alt="pause" />
+        <button className="hero__video-pause" ref={pause}>
+          <img src={pauseIcon} alt="pause" />
         </button>
         <input
           type="range"
           className="hero__video-progressBar"
-          value={0}
+          value={range}
           min={0}
           max={100}
           step={1}
         />
-        <button className="hero__video-fullscreen">
-          <img src={fullscreen} alt="fullscreen" />
+        <button className="hero__video-fullscreen" ref={fullscreen}>
+          <img src={fullscreenIcon} alt="fullscreen" />
         </button>
-        <button className="hero__video-close-fullscreen">
-          <img src={close_fullscreen} alt="close_fullscreen" />
+        <button className="hero__video-close-fullscreen" ref={close_fullscreen}>
+          <img src={close_fullscreenIcon} alt="close_fullscreen" />
         </button>
-        <button className="hero__video-scrub">
-          <img src={scrub} alt="scrub" />
+        <button className="hero__video-scrub" ref={scrub}>
+          <img src={scrubIcon} alt="scrub" />
         </button>
       </div>
     </section>
